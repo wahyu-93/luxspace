5d5d33553de64d9074de5f6b5181dfa11968fda3
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2022-06-19 02:38:11 +0800

    ganti logo jetstream dan create middleware [done]

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [FrontController::class, 'index'])->name('index');
-Route::get('/details/{slug}', [FrontController::class, 'detail'])->name('details');
-Route::get('/cart', [FrontController::class, 'cart'])->name('cart');\
-Route::get('/checkout/succes', [FrontController::class, 'success'])->name('checkout-success');
+Route::middleware('admin')->group(function(){
+    Route::get('/', [FrontController::class, 'index'])->name('index');
+    Route::get('/details/{slug}', [FrontController::class, 'detail'])->name('details');
+    Route::get('/cart', [FrontController::class, 'cart'])->name('cart');\
+    Route::get('/checkout/succes', [FrontController::class, 'success'])->name('checkout-success');
+});
+
 
 Route::middleware([
     'auth:sanctum',
